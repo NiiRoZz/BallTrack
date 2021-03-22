66c9b0eba230a521fe6dc455b54e121fac4e9a18
NiiRoZz <extazziii1000@gmail.com>
2021-03-22 15:18:18 +0100

    Push basic

---
+++ BallTrack.cpp
@@ -0,0 +1,12 @@
+﻿// BallTrack.cpp : Defines the entry point for the application.
+//
+
+#include "BallTrack.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
